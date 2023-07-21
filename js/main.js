283fa94b920492dfea6e46cf3b8dd51c24ca874d
Drend <byronthyto@gmail.com>
2023-07-21 10:16:58 -0500

    Añadido menu desktop

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+
+menuEmail.addEventListener("click", toggleMenuDesktop);
+
+function toggleMenuDesktop(){
+    desktopMenu.classList.toggle("inactive")
+}
+
