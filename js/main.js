8fb62c225d8fa0fd8b63dbac76d62f6ddd3e8eaa
Drend <byronthyto@gmail.com>
2023-07-21 15:00:03 -0500

    Funcion en JS para que se agregen los productos al html de forma mas automatica sin llenar innecesariamente en el html

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const burguerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleMenuDesktop);
 burguerMenu.addEventListener("click", toggleMenuMobile);
@@ -41,3 +42,69 @@ function toggleAside(){
     // console.log("toogle aside");
 }
 
+const productList = []
+productList.push({
+    name: "Pochita 1",
+    price: "1 millon",
+    image: "../img/Pochita.PNG.webp"
+})
+productList.push({
+    name: "Pochita",
+    price: "1 millon",
+    image: "../img/Pochita.PNG.webp"
+})
+productList.push({
+    name: "Pochita",
+    price: "1 millon",
+    image: "../img/Pochita.PNG.webp"
+})
+productList.push({
+    name: "Pochita",
+    price: "1 millon",
+    image: "../img/Pochita.PNG.webp"
+})
+productList.push({
+    name: "Pochita",
+    price: "1 millon",
+    image: "../img/Pochita.PNG.webp"
+})
+
+function renderProducts (arr){
+    for (product of arr){
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.image);
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        const productInfoDiv = document.createElement("div")
+    
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" +product.price;
+    
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+        const productInfoFigure = document.createElement("figure")
+        const productImgCart = document.createElement("img")
+        productImgCart.setAttribute("src", "../icons/bt_add_to_cart.svg")
+    
+        productInfoFigure.appendChild(productImgCart)
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+    
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList)
