d35147f8ed3fa2a7cc51723fe20ea6e3aa73fdc8
Drend <byronthyto@gmail.com>
2023-07-21 16:07:15 -0500

    Agregado casi todos los casis posibles de cuando abrimos deferentes ventanas

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const burguerMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const productDetailCloseIcon = document.querySelector(".product-detail-close");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
+const productDetailContainer = document.querySelector("#productDetail");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleMenuDesktop);
 burguerMenu.addEventListener("click", toggleMenuMobile);
 menuCarritoIcon.addEventListener("click", toggleAside);
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside);
 
 function toggleMenuDesktop(){
      // Preguntamos si ese selector  no tiene dicha clase activa
@@ -17,6 +20,8 @@ function toggleMenuDesktop(){
      if(!isMobileMenuClosed){
          mobileMenu.classList.add("inactive")
      }
+
+    
      
      shoppingCartContainer.classList.toggle("inactive")
      // console.log("toogle aside");x
@@ -28,6 +33,9 @@ function toggleMenuMobile(){
     if(!isAsideClosed){
         shoppingCartContainer.classList.toggle("inactive")
     }
+
+    closeProductDetailAside()
+    
     mobileMenu.classList.toggle("inactive")
 }
 function toggleAside(){
@@ -38,10 +46,22 @@ function toggleAside(){
         mobileMenu.classList.add("inactive")
     }
     
+    const isProductDetailClosed = productDetailContainer.classList.contains("inactive")
+
+    if(!isProductDetailClosed){
+       productDetailContainer.classList.add("inactive")
+    }
     shoppingCartContainer.classList.toggle("inactive")
     // console.log("toogle aside");
 }
 
+function openProductInfo(){
+    shoppingCartContainer.classList.add("inactive")
+    productDetailContainer.classList.remove("inactive")
+}
+function closeProductDetailAside(){
+    productDetailContainer.classList.add("inactive")
+}
 const productList = []
 productList.push({
     name: "Pochita 1",
@@ -76,6 +96,7 @@ function renderProducts (arr){
     
         const productImg = document.createElement("img");
         productImg.setAttribute("src", product.image);
+        productImg.addEventListener("click", openProductInfo)
     
         const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
