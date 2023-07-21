1aa90a063f0be6b0ad8b1adb428528b4c876f697
Drend <byronthyto@gmail.com>
2023-07-21 13:33:36 -0500

    Guardamos cambios por si las moscas desde en inicio de la creacion de la lista de compras o algo asi

--- js/main.js
+++ js/main.js
@@ -1,9 +1,43 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleMenuDesktop);
+burguerMenu.addEventListener("click", toggleMenuMobile);
+menuCarritoIcon.addEventListener("click", toggleAside);
 
 function toggleMenuDesktop(){
+     // Preguntamos si ese selector  no tiene dicha clase activa
+     const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
+
+     if(!isMobileMenuClosed){
+         mobileMenu.classList.add("inactive")
+     }
+     
+      aside.classList.toggle("inactive")
+     // console.log("toogle aside");x
     desktopMenu.classList.toggle("inactive")
+}
+function toggleMenuMobile(){
+    const isAsideClosed = aside.classList.contains("inactive")
+
+    if(!isAsideClosed){
+        aside.classList.toggle("inactive")
+    }
+    mobileMenu.classList.toggle("inactive")
+}
+function toggleAside(){
+    // Preguntamos si ese selector  no tiene dicha clase activa
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive")
+    }
+    
+     aside.classList.toggle("inactive")
+    // console.log("toogle aside");
 }
 
